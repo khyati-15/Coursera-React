07e054248366026fac6c437341e3c06bfaac0ee9
Khyati Khanduja <khyati15khanduja@gmail.com>
2020-05-06 18:32:23 +0530

    Components Part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Khyati Khanduja</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state={
+      dishes : DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Khyati Khanduja</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
-
 export default App;
