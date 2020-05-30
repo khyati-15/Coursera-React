91f9dd04c4b52435850f7fd49899627a91efd117
Khyati Khanduja <khyati15khanduja@gmail.com>
2020-05-30 12:28:09 +0530

    Redux Thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import {Card,CardImg,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb,Button,Modal,ModalHeader,ModalBody,Row,Col,Label} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control,LocalForm,Errors} from 'react-redux-form';
-
+import {Loading} from './LoadingComponent'
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -169,7 +169,25 @@ const minLength = len => val => val && val.length >= len;
 
     const DishDetail = (props) =>{
         const dish = props.dish;
-        if(dish!=null){
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(dish!=null){
             return(
                 <div className="container">
                 <div className="row">
